8e7cdad116eebb4d6835bd91bfef1878239cd6fd
hugomartinez <hugo.martinez@supdepub.com>
2020-05-08 22:52:35 +0200

    Implementing Routes & Link Finalizing and fixing details

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -7,6 +7,7 @@ import {
     TiHeartFullOutline
 } from "react-icons/ti";
 import { handleToggleTweet } from "../actions/tweets";
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
     handleLike = (e) => {
@@ -20,10 +21,12 @@ class Tweet extends Component {
             authedUser
         }))
     }
+
     toParent = (e, id) => {
         e.preventDefault()
-        // todo: Redirect to parent Tweet.
+        this.props.history.push(`/tweet/${id}`)
     }
+
     render() {
         const { tweet } = this.props
 
@@ -32,11 +35,11 @@ class Tweet extends Component {
         }
 
         const {
-            name, avatar, timestamp, text, hasLiked, likes, replies, parent
+            name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
         } = tweet
 
         return (
-            <div className='tweet'>
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img
                     src={avatar}
                     alt={`Avatar of ${name}`}
@@ -64,7 +67,7 @@ class Tweet extends Component {
                         <span>{likes !== 0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -81,4 +84,4 @@ function mapStateToProps ({authedUser, users, tweets}, { id }) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
